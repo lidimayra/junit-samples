d780808e7851471b28be3bb9ce47ac256c739469
Lidiane Taquehara <lidi.mayra@gmail.com>
2019-01-28 22:19:03 -0200

    Verify params passed to URL in PlanetTest
    Add a guard class to the stub to ensure that search term
    is passed as param when calling getClimate().
    
    Now, the stub is close to a mock (an object that mimics the behavior
    of a testable part of the application in a controlled way and
    verifies whether or not it received the expected params).
    
    This has been done mannually in these changes. However, there is
    a general-purpose mock tool that could be used for situations like
    these. It is called Mockito.
    
    Ref: https://site.mockito.org/

--- src/test/java/PlanetTest.java
+++ src/test/java/PlanetTest.java
@@ -7,15 +7,18 @@ public class PlanetTest {
 
     @Test
     public void run() throws IOException {
-        Http http = (String url) ->
-            "{\"results\":" +
-                "[" +
-                    "{" +
-                        "\"name\": \"Tatooine\"," +
-                        "\"climate\": \"arid\"" +
-                    "}" +
-                "]" +
-            "}";
+        Http http = (String url) -> {
+            if(!url.contains("search=Tatooine"))
+                Assert.fail(url + " does not contain expected param!");
+            return "{\"results\":" +
+                        "[" +
+                            "{" +
+                                "\"name\": \"Tatooine\"," +
+                                "\"climate\": \"arid\"" +
+                            "}" +
+                        "]" +
+                    "}";
+        };
 
         Planet planet = new Planet(http);
         String climate = planet.getClimate("Tatooine");
